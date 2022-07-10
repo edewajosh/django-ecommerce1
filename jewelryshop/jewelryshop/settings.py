e0d98c8183087e888220bb5c63ea68d498d6b113
Vivvianne <ndiahav@gmail.com>
2022-07-10 23:21:26 +0300

    added media settings

--- jewelryshop/jewelryshop/settings.py
+++ jewelryshop/jewelryshop/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
 
+# Settings for Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
